fe4948f3e8dd1fa1b90ea1894a8f43ba8ebc2406
Vishal <vishalgrover.work@gmail.com>
2020-03-29 19:42:01 +0530

    Dynamic Burger Ingredients

--- React-Project/my-burger/src/components/Burger/Burger.js
+++ React-Project/my-burger/src/components/Burger/Burger.js
@@ -3,12 +3,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient.js'
 import burgerStyle from './Burger.module.css'
 
 const burger = (props) => {
+
+	let propsIngredients = Object.keys(props.ingredients).map(ingredientsKey => {
+		return [...Array(props.ingredients[ingredientsKey])].map((_, i) => {
+			return <BurgerIngredient type={ingredientsKey} key={ingredientsKey + "-" + i}/>
+		})
+	}).reduce((accumulator, currentValue) => {
+		return accumulator.concat(currentValue)
+	}, [])
+	if (propsIngredients.length === 0) {
+		propsIngredients = <p>Please Add Some Ingredients
+		</p>
+	}
 	return (<div className={burgerStyle.Burger}>
-		<BurgerIngredient type='bread-top'/>
-		<BurgerIngredient type='patty'/>
-		<BurgerIngredient type='cheese'/>
-		<BurgerIngredient type='salad'/>
-		<BurgerIngredient type='bacon'/>
+		<BurgerIngredient type='bread-top'/> {propsIngredients}
 		<BurgerIngredient type='bread-bottom'/>
 
 	</div>)
--- React-Project/my-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ React-Project/my-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,9 +3,18 @@ import Aux from './../../hoc/auxiliary.js'
 import Burger from './../../components/Burger/Burger.js'
 
 class BurgerBuilder extends React.Component {
+	state = {
+		ingredients: {
+			cheese: 0,
+			patty: 1,
+			salad: 0,
+			bacon: 0,
+		}
+	}
+
 	render() {
 		return (<Aux>
-			<Burger/>
+			<Burger ingredients={this.state.ingredients}/>
 			<div>Build Controls</div>
 		</Aux>)
 	}
