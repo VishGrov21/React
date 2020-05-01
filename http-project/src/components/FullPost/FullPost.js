454833c0349120a30822ee5ee64329fbd24bd2b5
Vishal <vishalgrover.work@gmail.com>
2020-05-01 11:19:08 +0530

    Restful Service Demo

--- http-project/src/components/FullPost/FullPost.js
+++ http-project/src/components/FullPost/FullPost.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+    state = {
+        updatedPost: null
+    }
 
-        );
+    componentDidUpdate() {
+        if (this.props.id) {
+            if (!this.state.updatedPost || (this.state.updatedPost &&
+                this.state.updatedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        console.log(response);
+                        this.setState({ updatedPost: response.data })
+                        console.log(this.state.updatedPost)
+                    })
+            }
+        }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            })
+    }
+    render() {
+        let post = <p>Please select a Post!</p>;
+        if (this.props.id) {
+            post = <p>Loading...</p>;
+        }
+        if (this.state.updatedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.updatedPost.title}</h1>
+                    <p>{this.state.updatedPost.body}</p>
+                    <div className="Edit">
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
+                    </div>
+                </div>
+
+            );
+        }
+
         return post;
     }
 }
--- http-project/src/containers/Blog/Blog.js
+++ http-project/src/containers/Blog/Blog.js
@@ -4,18 +4,35 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios'
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: [],
+        clickedPost: null
+    }
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(response => {
+                const posts = response.data.slice(0, 4)
+                this.setState({ posts: posts })
+            })
+    }
+    clickedPostHandler(id) {
+        this.setState({ clickedPost: id })
+        console.log(this.state)
+    }
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post title={post.title} key={post.id} click={() => this.clickedPostHandler(post.id)} />
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.clickedPost} />
                 </section>
                 <section>
                     <NewPost />
